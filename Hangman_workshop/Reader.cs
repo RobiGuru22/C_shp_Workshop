c7a4b8a630fe21b392f58ac9a935860d501a70e7
RobiGuru22 <hoorvath.robert@gmail.com>
2024-06-17 09:57:04 +0200

    Hangman:Project folder added
    Hangman_worshop mappa hozzáadva, hogy program futtatható legyen

---
+++ Hangman_workshop/Reader.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman_workshop
+{
+    public class Reader
+    {
+        public static string GetWord(string path, int difficulty)
+        {
+            List<string> words = File.ReadAllLines(path).ToList();
+            Random rnd = new Random();
+            if(difficulty == 1)
+            {
+                List<string> rightWords = words.Where(x => x.Length <= 5).ToList();
+                return rightWords[rnd.Next(0, rightWords.Count())];
+            }
+            else if (difficulty == 2)
+            {
+                List<string> rightWords = words.Where(x => x.Length > 5 && x.Length <= 10).ToList();
+                return rightWords[rnd.Next(0, rightWords.Count())];
+            }
+            else
+            {
+                List<string> rightWords = words.Where(x => x.Length > 10).ToList();
+                return rightWords[rnd.Next(0, rightWords.Count())];
+            }
+        }
+    }
+}
