230a3a70294fd8d3657f9744e978497294e04cd0
RobiGuru22 <hoorvath.robert@gmail.com>
2024-06-17 10:13:17 +0200

    BugFix:Reader.cs
    Privát metúdusok a nehézség választáshoz

--- Hangman_workshop/Reader.cs
+++ Hangman_workshop/Reader.cs
@@ -8,24 +8,39 @@ namespace Hangman_workshop
 {
     public class Reader
     {
+        private static string EasyDifficulty(List<string> words)
+        {
+            Random rnd = new Random();
+            List<string> rightWords = words.Where(x => x.Length <= 5).ToList();
+            return rightWords[rnd.Next(0, rightWords.Count())];
+        }
+        private static string MediumDifficulty(List<string> words)
+        {
+            Random rnd = new Random();
+            List<string> rightWords = words.Where(x => x.Length > 5 && x.Length <= 10).ToList();
+            return rightWords[rnd.Next(0, rightWords.Count())];
+        }
+        private static string HardDifficulty(List<string> words)
+        {
+            Random rnd = new Random();
+            List<string> rightWords = words.Where(x => x.Length > 10).ToList();
+            return rightWords[rnd.Next(0, rightWords.Count())];
+        }
         public static string GetWord(string path, int difficulty)
         {
             List<string> words = File.ReadAllLines(path).ToList();
-            Random rnd = new Random();
+            
             if(difficulty == 1)
             {
-                List<string> rightWords = words.Where(x => x.Length <= 5).ToList();
-                return rightWords[rnd.Next(0, rightWords.Count())];
+                return EasyDifficulty(words);
             }
             else if (difficulty == 2)
             {
-                List<string> rightWords = words.Where(x => x.Length > 5 && x.Length <= 10).ToList();
-                return rightWords[rnd.Next(0, rightWords.Count())];
+                return MediumDifficulty(words);
             }
             else
             {
-                List<string> rightWords = words.Where(x => x.Length > 10).ToList();
-                return rightWords[rnd.Next(0, rightWords.Count())];
+                return HardDifficulty(words);
             }
         }
     }
