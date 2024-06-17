ff6312c92cf1cbcf13d92cc552867de48ed97579
RobiGuru22 <hoorvath.robert@gmail.com>
2024-06-17 14:39:11 +0200

    RepoClear:Doc clearing
    Hangman_workshop feletti .cs-k törlése, mert mappán belülik lettek frissítve

--- Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using Hangman_workshop;
-using System.Diagnostics;
-using System.IO;
-
-string path = "C:\\Users\\HorvathR\\Desktop\\C# workshop\\Hangman_workshop\\words.txt";
-
-Game.Run(Window.MainWindow(), path);
